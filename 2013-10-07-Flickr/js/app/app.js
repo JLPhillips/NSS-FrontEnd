6026fb5b0fb6ee29aa7d5862edaf31fe1c5fb922
JL Phillips <phillipsj@pop.belmont.edu>
2013-10-08 09:28:44 -0500

    More labs.

--- 2013-10-07-Flickr/js/app/app.js
+++ 2013-10-07-Flickr/js/app/app.js
@@ -5,11 +5,16 @@ $(document).ready(initialize);
 function initialize(){
   $(document).foundation();
   $("#search").click(searchFlickr);
+  $("#photos").on("dblclick", ".photo", dblClickRemove);
+  $("#photos").on("click", ".photo", addBorder);
+  $("#clearbutton").click(clear);
+  $("#deletebutton").click(deletePhotos);
+  $("#savebutton").click(savePhotos);
 }
 
 function searchFlickr(){
   var API_KEY = "b3b2f15713834c6af3bf843c46ade5c9";
-  var PER_PAGE = 30;
+  var PER_PAGE = 12;
   var page = 1;
 
   var query = $("#query").val();
@@ -30,3 +35,40 @@ function createImage(photo){
   $div.css("background-image", url);
   $("#photos").prepend($div);
 }
+
+function dblClickRemove(){
+  $(this).remove();
+}
+
+function clear(){
+  var $clear = $("#photos");
+  $clear.empty();
+  $("#query").focus();
+  alert("Deleted!");
+}
+
+function deletePhotos(){
+  $(".photoborder").remove();
+}
+
+function savePhotos(){
+  var $selected = $(".photoborder");
+  $selected.removeClass("photoborder");
+  $("#saved_photos").prepend($selected);
+}
+
+function addBorder(){
+  var $this = $(this);
+  if ($this.hasClass("photoborder")){
+    $this.removeClass("photoborder");
+  }
+  else{
+    $this.addClass("photoborder");
+  }
+}
+
+// HOW CHYLD DID THIS^^^^^^
+
+// function addBorder(){
+//   $(this).toggleClass("selected");
+// }
